96c6c0005f8c4a852f997c1ec771c55fa6237fa1
srihemainfotech <help@cwd.co.in>
2022-09-17 18:03:16 +0530

    two api get one screen

--- lib/screens/little_big_single_api.dart
+++ lib/screens/little_big_single_api.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:retry_build_api/model/little_big_model.dart';
+import 'package:retry_build_api/model/single_api_model.dart';
 
 class LittleBigApi extends StatefulWidget {
   const LittleBigApi({super.key});
@@ -15,21 +16,39 @@ class LittleBigApi extends StatefulWidget {
 
 class _LittleBigApiState extends State<LittleBigApi> {
   LittleBig nasted = LittleBig(data: Data(), support: Support());
+  SingleApi nasted2 = SingleApi();
   bool isLoaded = false;
   getData() async {
     var required = await http.get(
       Uri.parse("https://reqres.in/api/users/2"),
     );
+    var required2 = await http.get(
+      Uri.parse("http://0.0.0.0:3001/single_api"),
+    );
     var response = json.decode(required.body);
+    var response2 = json.decode(required2.body);
     setState(() {
       nasted = LittleBig.fromJson(response);
+      nasted2 = SingleApi.fromJson(response2);
       isLoaded = true;
     });
   }
 
+  // getData2() async {
+  //   var required = await http.get(
+  //     Uri.parse("http://0.0.0.0:3001/single_api"),
+  //   );
+  //   var response = json.decode(required.body);
+  //   setState(() {
+  //     nasted2 = SingleApi.fromJson(response);
+  //     isLoaded = true;
+  //   });
+  // }
+
   @override
   void initState() {
     getData();
+    // getData2();
     super.initState();
   }
 
@@ -44,7 +63,9 @@ class _LittleBigApiState extends State<LittleBigApi> {
                   children: [
                     Image.network("${nasted.data.avatar}"),
                     Text("${nasted.data.firstName}"),
-                    Text("${nasted.support.url}")
+                    Text("${nasted.support.url}"),
+                    Text("${nasted2.name}"),
+                    Text("${nasted2.phoneNumber}")
                   ],
                 )
               : CircularProgressIndicator(),
